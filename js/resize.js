7b5fce699e72cfce1804fb7d6b405ac6386487d2
Jussi Saravuo <jussisaravuo@Jussis-MacBook-Air.local>
2017-09-12 11:06:16 +0300

    Call handleZoom-function while resizing so that map will set correctly to the map-view.

--- js/resize.js
+++ js/resize.js
@@ -3,9 +3,13 @@
  *	Resize.
  */
 function resize() {
+	
 	// update mapHolder.
 	let width = window.innerWidth - mainConfig.maskOffsets.left - mainConfig.maskOffsets.right;
 	let height = window.innerHeight - mainConfig.maskOffsets.top - mainConfig.maskOffsets.bottom;
 	mapHolder.style.width = width + 'px';
 	mapHolder.style.height = height + 'px';
+
+	// Make sure that map will filll the whole map-view area.
+	handleZoom(1);
 }
